c869371d923e15eb1601480c77401273cb310d5a
Iñaki Narciso <1917005+iomusashi@users.noreply.github.com>
2023-11-12 15:39:33 +0800

    Module 01, Lesson 02: Writing a Simple Unit Test

---
+++ module-01/lesson-02/CWCUnitTesting/CWCUnitTesting/CWCUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWCUnitTestingApp.swift
+//  CWCUnitTesting
+//
+//  Created by ヒットり on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CWCUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
